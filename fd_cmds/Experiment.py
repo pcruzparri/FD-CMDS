fixed sign extraction from phasematching which was causing the first interaction frequency to be zero. The 1D scans now work as expected. Testing implementation of the 2d scan but spectra does not look as expected. Not working yet, just saving.

fd_cmds/Experiment.py
@@ -78,7 +78,7 @@ class Experiment:
         :return: output coherence amplitude
         """
 
-        signs, orderings = np.sign(self.pm), np.abs(self.pm)
+        signs, orderings = np.sign(self.pm), np.abs(self.pm)-1
         GROUND_STATE_GAMMA = _trans.wntohz(1e-18)
 
         t1 = self.transitions[0](self.rabis[orderings[0]],
@@ -126,6 +126,7 @@ class Experiment:
                                self.gammas[orderings[2]],
                                np.linspace(0, self.times[5]-self.times[4],
                                            int((self.times[5]-self.times[4])*time_int)+1))
+                                           
         coeff = t1*fid1*t2*fid2
         out_field = coeff*t3
         return np.sum(np.real(out_field*np.conjugate(out_field)))

fd_cmds/script.py
@@ -25,22 +25,52 @@ exp.set_pws(pws)
 exp.set_times()
 exp.set_pulse_freqs([2253, 3164, omegas[2]])
 exp.set_transitions([_trans.bra_abs, _trans.ket_abs, _trans.ket_abs])
-exp.set_pm([-0, 1, 2])
+exp.set_pm([-1, 2, 3])
 
-shape = (20, 20)
 
 e = exp.compute()
 print(e)
 
-w1_scan = np.linspace(-200, 200, 100)
+def f(w1,w2):
+    omegas = [2253, 3164, 77000] #in wn for the individual states.
+    gammas = [20, 50, 1000] #in wn for the coherences, not the individual states.
+    rabis = [5, 8, 1000] #in wn for the coherences, by definition. 
+    exp = Experiment(omegas, gammas, rabis)
+
+    d1 = 0
+    d2 = 0
+    delays = [d1, d2]
+    exp.set_delays(delays)
+
+    pw1 = 500e-15
+    pw2 = 500e-15
+    pw3 = 500e-15
+    pws = [pw1, pw2, pw3]
+    exp.set_pws(pws)
+    exp.set_times()
+    exp.set_pulse_freqs([w1*5+2100, w2*5+3000, omegas[2]])
+    exp.set_transitions([_trans.bra_abs, _trans.ket_abs, _trans.ket_abs])
+    exp.set_pm([-1, 2, 3])
+    print(w1,w2)
+    return exp.compute()
+        
+matrix = np.fromfunction(np.vectorize(f), (40,40), dtype=float)
+plt.imshow(np.transpose(matrix))
+plt.show()
+
+
+"""
+w1_scan = np.linspace(2200, 2300, 100)
+w2_scan = np.linspace(3000, 3200, 100)
 out_scan = []
-for i in w1_scan:
+for i in w2_scan:
     print(i)
-    exp.set_pulse_freqs([i, 3164, omegas[2]])
+    exp.set_pulse_freqs([2253, i, omegas[2]])
     out_scan.append(exp.compute())
 
-plt.plot(w1_scan, out_scan)
+plt.plot(w2_scan, out_scan)
 plt.show()
+"""
 
 """
 n_threads = 20 #ensure it is multiple of w2 scan width